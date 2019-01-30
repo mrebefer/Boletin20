eb52a68fe35edc607175a5ea39f14e19d9a16a7d
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2019-01-30 12:19:32 +0100

    first commit

---
+++ Boletin20_1C/src/boletin20_1c/Boletin20_1C.java
@@ -0,0 +1,32 @@
+/*A seguinte táboa recolle los goles marcados polos 20 equipos de 1ª  división 
+*en cada unha das 36 xornadas. 
+*Hacer con un menú y switch case.
+*Equipo/xornada    X1   X2     X3 ……
+*        Celta    3     4        2
+*        Depor    1     2        1 
+*        Málaga   3     0        1
+*
+* Pídese: 
+* 20_1A Gravar e imprimir a táboa. 
+* 20_1B Obter unha listaxe dos equipos por orde de menor n° de goles na liga. 
+* 20_1C Indicar o equipo que marca máis goles en cada xornada. 
+* 20_1D Que equipo e en que xornada se rexistran máis goles? 
+* 20_1E Consultas por equipo e xornada indicando o n° de goles. 
+*
+*/
+package boletin20_1c;
+
+/**
+ *
+ * @author mrebelladofernandez
+ */
+public class Boletin20_1C {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
